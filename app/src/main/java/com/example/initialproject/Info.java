11f3c4a31f19b3e79fcff132e2b58da354ebea75
Liyh <liyuhan0@tju.edu.cn>
2018-10-29 00:22:33 +0800

    Add the network.

--- app/src/main/java/com/example/initialproject/Info.java
+++ app/src/main/java/com/example/initialproject/Info.java
@@ -3,10 +3,16 @@ package com.example.initialproject;
 public class Info {
     private String name;
     private int imageName;
+    private int ID;
+    private int type;
+    private int date;
 
-    public Info(String name, int imageName) {
-        this.imageName=imageName;
-        this.name=name;
+    public Info(String name, int imageName, int type, int ID,int date) {
+        this.imageName = imageName;
+        this.name = name;
+        this.type = type;
+        this.ID = ID;
+        this.date=date;
     }
 
     public String getName() {
@@ -15,5 +21,17 @@ public class Info {
 
     public int getImageName() {
         return imageName;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getDate() {
+        return date;
     }
 }
--- app/src/main/java/com/example/initialproject/MainActivity.java
+++ app/src/main/java/com/example/initialproject/MainActivity.java
@@ -4,24 +4,63 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.LinearLayout;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Info> myList = new ArrayList<>();
 
-    public void init() {
-
+    public void init() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("http://news-at.zhihu.com/api/3/news/latest").build();
+        Log.d("Impor","233");
+        String responseData;
+        String title;
+        Log.d("Impor","2334");
+        Response response = client.newCall(request).execute();
+        Log.d("Impor","2335");
+        assert response.body() != null;
+        responseData = response.body().toString();
+        Log.d("Impor", responseData);
+        Info info;
+        try {
+            JSONObject jsonObject = new JSONObject(responseData);
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("stories"));
+            int date = jsonObject.getInt("date");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                int id = object.getInt("id");
+                title = object.getString("title");
+                int type = object.getInt("type");
+                info = new Info(title, R.drawable.temp, type, id, date);
+                myList.add(info);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Impor","The program is running");
         setContentView(R.layout.mainlayout);
-        init();
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
